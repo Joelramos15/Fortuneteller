4d0f16719a05938a5efdea4c2a3e03db9890662a
Joelramos15 <ramos8910@yahoo.com>
2017-02-15 20:20:51 -0500

    first commit

---
+++ FortuneTeller/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastname;
+            int userAge;
+            int userBirthMonth;
+            string userFavoriteColor;
+            int numberOfSiblings;
+
+            Console.WriteLine("what is your first name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine("what is your last name?");
+            lastname = Console.ReadLine();
+            Console.WriteLine("what is your age?");
+            int Age = Console.ReadLine();
+            Console.WriteLine("what is your birth month?");
+            userBirthMonth = Console.ReadLine();
+            Console.WriteLine("what is your favorite color");
+            userFavoriteColor = Console.ReadLine();
+
+            bool enterHelp = true;
+            if (userdoesNotKnowColor)
+            {
+                Console.WriteLine(" type help");
+            }
+            Console.WriteLine("how many siblings do you have?");
+            numberOfSiblings = Console.WriteLine();
+
+            bool odd = true;
+            if (userAge == "odd")
+
+            {
+                Console.WriteLine("retire in five Years");
+            }
+            else
+            {
+                Console.WriteLine(" retire in 10 years");
+            }
+            {
+                int[] vacationHomes;
+            int[] vacationHomes = new int{ 6};
+
+            vacationHomes [index]= { "0,1,2,3,>3, >=0"};
+
+            vacationHomes[0] = 1;
+            vacationHomes[1] = 2;
+            vacationHomes[2] = 3;
+            vacationHomes[3] =>3;
+            vacationHomes[4] =>= 0;
+
+            Console.Write(vacationHomes);
+        }
+            {
+                string[] colors;
+                string[] colors = new string { 7 };
+
+                colors[index] = {"boat,car,bike, plane, motorcycle, skates, jetpack,"};
+
+                colors[0] = boat;
+                colors[1] = car;
+                colors[2] = bike;
+                colors[3] = plane;
+                colors[4] = motorcycle;
+                colors[5] = skates;
+                colors[6] = jetpack;
+
+                Console.Write(colors); }
+            {
+
+                int[] savings;
+                int[] savings = new int { "3" };
+
+                savings[index] = { "100,50,25,0"};
+
+                savings[0] = 100;
+                savings[1] = 50;
+                savings[2] = 25;
+                savings[3] = 0;
+
+                Console.Write(savings);
+            }
+
+   
+}
